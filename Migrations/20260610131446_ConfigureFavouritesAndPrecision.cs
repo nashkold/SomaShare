0770e371881930ba1d8c91df3490caaa220e2bdc
Alex Gonese <25301757@stadio.ac.za>
2026-06-10 15:32:18 +0200

    Updated the database and commented out DropIndex to resolve EF Core error due to Git rollback

---
+++ Migrations/20260610131446_ConfigureFavouritesAndPrecision.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SomaShare.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigureFavouritesAndPrecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Favourites_Textbooks_TextbookId",
+                table: "Favourites");
+            //
+            //migrationBuilder.DropIndex(
+            //    name: "IX_Favourites_UserId",
+            //    table: "Favourites");
+            //
+            migrationBuilder.CreateIndex(
+                name: "IX_Favourites_UserId_TextbookId",
+                table: "Favourites",
+                columns: new[] { "UserId", "TextbookId" },
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Favourites_Textbooks_TextbookId",
+                table: "Favourites",
+                column: "TextbookId",
+                principalTable: "Textbooks",
+                principalColumn: "TextbookId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Favourites_Textbooks_TextbookId",
+                table: "Favourites");
+            //
+            //migrationBuilder.DropIndex(
+            //    name: "IX_Favourites_UserId_TextbookId",
+            //    table: "Favourites");
+            //
+            migrationBuilder.CreateIndex(
+                name: "IX_Favourites_UserId",
+                table: "Favourites",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Favourites_Textbooks_TextbookId",
+                table: "Favourites",
+                column: "TextbookId",
+                principalTable: "Textbooks",
+                principalColumn: "TextbookId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
